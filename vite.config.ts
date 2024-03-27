8a42f7fe0bc6c3b927010d99f1e16d6377a9b0c7
Nik <Nik96.ge@gmail.com>
2024-03-27 14:41:46 +0330

    Add Vite Alias.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    // alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
   plugins: [react()],
 })
