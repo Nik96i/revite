11f75913d2c9b8b3d28046b9182f5b5370823581
Nik <Nik96.ge@gmail.com>
2024-09-09 09:20:14 +0330

    Add dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from "node:url"
+import { defineConfig, loadEnv } from "vite";
+import type { ProxyOptions } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
-  },
-  resolve: {
-    // alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  const proxyConfigs: Record<string, string | ProxyOptions> = {
+    "/api/": {
+      secure: false,
+      target: env.VITE_DEV_SERVER,
+      changeOrigin: true
+    }
+  };
+
+  return {
+    define: {
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
     },
-  },
-  plugins: [react()],
-})
+    preview: {
+      proxy: proxyConfigs
+    },
+    server: {
+      proxy: proxyConfigs
+    },
+    resolve: {
+      // alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    },
+    plugins: [react()]
+  };
+});
